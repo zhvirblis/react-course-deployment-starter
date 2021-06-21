41bc4c7844f95cc7390b11cbf9c151d01a01598c
mitchelst <mitchel@bendingtherules.nl>
2021-06-21 03:49:54 +0200

    add: starter code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from './App';
 
-test('renders learn react link', () => {
+test('if user enters a todo, the todo is shown', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // login
+  userEvent.click(screen.getByRole('button', { name: /click here to login/i }));
+
+  // add todo
+  userEvent.type(screen.getByRole('textbox', { name: /title/i }), "buy new shoes");
+  userEvent.click(screen.getByRole('button', { name: /submit/i }));
+
+  expect(await screen.findByText(/buy new shoes/i)).toBeInTheDocument();
 });
